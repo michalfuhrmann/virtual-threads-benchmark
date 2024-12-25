81b6fbff21a75f07716f6d10bb8a03e4a9c324d1
Michal <torosmg@gmail.com>
2024-12-25 18:25:46 +0100

    cleanup

--- src/main/java/com/mfuhrmann/benchmarks/VirtualThreadsBenchmarkMain.java
+++ src/main/java/com/mfuhrmann/benchmarks/VirtualThreadsBenchmarkMain.java
@@ -11,28 +11,22 @@ import com.mfuhrmann.benchmarks.executors.PhysicalThreadPerTaskExecutor;
 import com.mfuhrmann.benchmarks.executors.VirtualThreadsPerTaskExecutor;
 import com.mfuhrmann.benchmarks.workloads.OperationType;
 
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Set;
 import java.util.concurrent.ThreadLocalRandom;
 
-import static com.mfuhrmann.benchmarks.workloads.OperationType.CPU_HEAVY;
-import static com.mfuhrmann.benchmarks.workloads.OperationType.MIXED;
-import static com.mfuhrmann.benchmarks.workloads.OperationType.NETWORK;
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
 
-public class JavaVolatilityCalculationBenchmarkMain {
+public class VirtualThreadsBenchmarkMain {
 
     public static final double MAX_STOCK_PRICe = 250.0;
     public static final double MIN_STOCK_PRICE = 50.0;
     public static final int NUM_SAMPLES = 200_000;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         var optionInputData = SyntheticDataGenerator.generateSyntheticData(NUM_SAMPLES, MIN_STOCK_PRICE, MAX_STOCK_PRICe);
 
-        Stopwatch stopwatch1 = Stopwatch.createStarted();
         Arrays.stream(OperationType.values())
 //                .filter(operationType -> Set.of(CPU_HEAVY).contains(operationType))
                 .forEach(operationType -> {
@@ -40,7 +34,6 @@ public class JavaVolatilityCalculationBenchmarkMain {
                         Stopwatch stopwatch = Stopwatch.createStarted();
                         var optionPriceResponse = executor.execute(optionInputData, operationType);
                         System.out.println("Operation: " + operationType + " - Executor - " + executor + " Time taken: " + stopwatch.elapsed(MILLISECONDS) + " ms" + "    consume output: " + optionPriceResponse.size());
-//            printResultsSnapshot(optionPriceResponse);
                     });
 
                 });
